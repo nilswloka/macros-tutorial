0ee69152afca1ff0e380b6b4a52f4d263b93f190
dnolen <david.nolen@gmail.com>
2010-03-16 22:37:40 -0400

    * readme.textile: working.

---
+++ src/tutorial/books.clj
@@ -0,0 +1,64 @@
+(ns tutorial.books
+  (:use compojure
+        [clojure.contrib.duck-streams :only [pwd]]
+        [tutorial.utils :only [render render-snippet]])
+  (:require [net.cgrand.enlive-html :as html]
+            [tutorial.macroology :as m]))
+
+(def *resources* (str (pwd) "/src/resources/"))
+
+(def *books* [{:title "Gravity's Rainbow"
+               :author "Thomas Pynchon"
+               :publisher "Viking Press"
+               :date "1973"
+               :edition "1st"
+               :ISBN "?"}
+              {:title "Life A User's Manual"
+               :author "Georges Perec"
+               :publisher "Hachette Littératures"
+               :date "1978"
+               :edition "1st"
+               :ISBN "?"}
+              {:title "2666"
+               :author "Roberto Bolaño"
+               :publisher "Editorial Anagrama"
+               :date "2004"
+               :edition "1st"
+               :ISBN "978-8433968678"}])
+
+;; =============================================================================
+;; This is it
+;; =============================================================================
+
+(m/quick-snippet book "resources/widgets.html" [:.book])
+
+(html/deftemplate index "resources/index.html"
+  [ctxt]
+  [:.books] (html/content (map book (:books ctxt))))
+
+;; =============================================================================
+;; Routes
+;; =============================================================================
+
+(defroutes app-routes
+  (GET "/"
+       (render (index {:books *books*})))
+  (GET "*/main.css"
+       (serve-file *resources* "main.css"))
+  (ANY "*"
+       [404 "Page Not Found"]))
+
+;; =============================================================================
+;; The App
+;; =============================================================================
+
+(defonce *app* (atom nil))
+
+(defn start-app []
+  (if (not (nil? @*app*))
+    (stop @*app*))
+  (reset! *app* (run-server {:port 8080}
+                            "/*" (servlet app-routes))))
+
+(defn stop-app []
+  (when @*app* (stop @*app*)))
--- src/tutorial/macroology.clj
+++ src/tutorial/macroology.clj
@@ -1,20 +1,18 @@
 (ns tutorial.macroology
   (:use [tutorial.utils :only [render]])
-  (:require [net.cgrand.enlive-html :as html])
-  (:import [java.io ByteArrayInputStream]))
+  (:require [net.cgrand.enlive-html :as html]
+            [clojure.contrib.str-utils2 :as s]))
 
-(def *nodes-with-class* (html/selector #{[[:* (html/attr? :class)]]}))
-
-(defn- to-in-s [str] (ByteArrayInputStream. (.getBytes str "UTF-8")))
+(def *child-nodes-with-class* (html/selector [html/root [:* (html/attr? :class)]]))
  
 (defn- selector-for-node [{tag :tag, attrs :attrs}]
-  (let [css-class (:class attrs)]
+  (let [css-class (s/trim (first (s/split (:class attrs) #" ")))]
     `([~(keyword (str (name tag) "." css-class))] (html/content (~(keyword css-class) ~'ctxt)))))
  
 (defmacro quick-snippet [name rsrc selector]
   (let [nodes (-> (html/html-resource (eval rsrc))
-                  (html/select (html/selector (eval selector)))
-                  (html/select *nodes-with-class*))]
+                  (html/select (eval `(html/selector ~selector)))
+                  (html/select *child-nodes-with-class*))]
     `(html/defsnippet ~name ~rsrc ~selector
        [~'ctxt]
        ~@(reduce concat (map selector-for-node nodes)))))
